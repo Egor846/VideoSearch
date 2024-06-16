768ed5a87b0a11288e79f3907ca5d5da998957c5
Egor Girko <e.girko@ya.ru>
2024-06-16 15:48:45 +0300

    homeWorkModule20

--- app/src/main/java/com/example/videosearch/MainActivity.kt
+++ app/src/main/java/com/example/videosearch/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.videosearch
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,36 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        initMenuButtons()
+
+    }
+
+
+    fun initMenuButtons() {
+        val menuButton: Button = findViewById(R.id.menu)
+        menuButton.setOnClickListener {
+            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+        }
+
+        val favorites: Button = findViewById(R.id.favorites)
+        favorites.setOnClickListener {
+            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+        }
+        val posmotretPozzhe: Button = findViewById(R.id.posmotret_pozzhe)
+        posmotretPozzhe.setOnClickListener {
+            Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+        }
+
+        val podborki: Button = findViewById(R.id.podborki)
+        podborki.setOnClickListener {
+            Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+        }
+
+        val settings: Button = findViewById(R.id.settings)
+        settings.setOnClickListener {
+            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
         }
     }
 }
